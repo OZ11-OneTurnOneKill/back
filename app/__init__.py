995d9e616ad88c5c2d726bc9008d4dc830874904
KIM SHINEDAY <kimshineday99@gmail.com>
2025-08-08 10:43:57 +0900

    google login : 구글 로그인 페이지 연결 	app/__init__.py 	app/__pycache__/__init__.cpython-313.pyc

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from fastapi.responses import ORJSONResponse
 from app.configs.tortoise_config import initialize_tortoise
 from app.apis.google_login import router as google_login
 from app.apis.community_router import router as community_router
+from app.apis.google_login import router as google_login
 
 app = FastAPI(default_response_class=ORJSONResponse)
 app.include_router(community_router)
